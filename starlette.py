8e667ccfacd74747dc4ed2321452173c9c223a1f
mm3023 <mikemoore227@gmail.com>
2024-11-17 21:34:02 -0500

    Update starlette.py

--- starlette.py
+++ starlette.py
@@ -1,4 +1,27 @@
 #fast ngin starlellte
 import starlette
 import uvicorn
+
+
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route, Router
+
 print("light on inside")
+
+app = Starlette()
+router = Router()
+@router.on_event("startup")
+async def connect_to_database():
+    # Perform database connection logic here
+    print("Database connected on startup")
+
+
+
+@router.get("/")
+async def homepage(request):
+    return JSONResponse({"message": "Hello World!"})
+
+
+
+app.add_routes(router)
