8aeb5b3f1d86e99a5f6f0adc5bc02d3ad3c2de22
mm3023 <mikemoore227@gmail.com>
2024-11-17 21:45:38 -0500

    Update starlette.py

--- starlette.py
+++ starlette.py
@@ -3,7 +3,7 @@
 import uvicorn
 
 
-from starlette.applications import Starlette
+#from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route, Router
 from starlette.requests import Request
