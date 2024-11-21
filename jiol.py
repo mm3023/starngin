526c921bba30886e11bd557e971708b809409742
mm3023 <mikemoore227@gmail.com>
2024-11-21 16:09:27 -0500

    Update jiol.py

--- jiol.py
+++ jiol.py
@@ -5,23 +5,23 @@ os.environ['PYTHONPATH'] = '/zenviroment/bin'
 
 
 
-#import sys
-#import os # if you want this directory
+
+
+
+
+
+
+'''
 
 try:
     sys.path.index('/zenviroment/bin') # Or os.getcwd() for this directory
 except ValueError:
     sys.path.append('/zenviroment/bin')
-
 '''
-from fastapi import FastAPI
-
-app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+
 '''
 
 
@@ -55,6 +55,9 @@ app = Starlette(debug=True, routes=[
 ])
 
 '''
+
+
+
 async def app(scope, receive, send):
     assert scope['type'] == 'http'
 
@@ -74,7 +77,7 @@ async def app(scope, receive, send):
     import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 async def app(scope, receive, send):
