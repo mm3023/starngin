2218b273dee5fbc5b2170775cd0d2ccd1cf3714a
mm3023 <mikemoore227@gmail.com>
2024-11-22 09:43:49 -0500

    Update jiol.py

--- jiol.py
+++ jiol.py
@@ -30,6 +30,16 @@ app = Starlette(debug=True, routes=[
     Route('/', homepage),
 ])
 
+
+
+
+
+
+app = App()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+
 '''
 
 
